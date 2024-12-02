d30cf8d9cee5b1c6b3c2f0eb78dfc8d131e639d4
Sviatoslav Shkut <sgshkut@gmail.com>
2024-12-02 23:10:57 +0100

    day 1 part 1

---
+++ 2024/rust/day-1/src/bin/part1.rs
@@ -0,0 +1,10 @@
+use day_1::part1::process;
+use miette::Context;
+
+fn main() -> miette::Result<()> {
+    let file = include_str!("../../input1.txt");
+    let result = process(file).context("process part 1")?;
+    println!("{}", result);
+    Ok(())
+
+}
---
+++ 2024/rust/day-1/src/part1.rs
@@ -0,0 +1,34 @@
+use std::iter::zip;
+
+pub fn process(input: &str) -> miette::Result<String> {
+    let mut left = vec![];
+    let mut right = vec![];
+
+    for line in input.lines() {
+        let mut items = line.split_whitespace();
+        left.push(items.next().unwrap().parse::<i32>().unwrap());
+        right.push(items.next().unwrap().parse::<i32>().unwrap());
+    }
+    left.sort();
+    right.sort();
+    let result: i32 = zip(left, right).map(|(l, r)| (l - r).abs()).sum();
+
+    Ok(result.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::part1::process;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        assert_eq!("11", process(input)?);
+        Ok(())
+    }
+}
